0a21d99d3e3566ed543e9d23c3422e84a4345a23
reddy <reddyramareddy07@gmail.com>
2026-02-03 17:46:31 +0530

    Backend work almost done

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+import {pool} from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+pool.on("connet", ()=>{
+    console.log("Connected to the database");
+})
+
+pool.on("error", (err) =>{
+    console.error("Unexpected error on idle client",err);
+});
+
+export default pool;
